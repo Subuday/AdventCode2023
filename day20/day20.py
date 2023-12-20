2ae5a55710eec1577435f2833e924d55ee2e7ea1
Maksym Sutkovenko <msutkovenko@readdle.com>
2023-12-20 20:43:34 +0200

    Solve day20 part2

--- day20/day20.py
+++ day20/day20.py
@@ -1,4 +1,5 @@
 import collections
+import math
 
 
 def part1():
@@ -73,7 +74,78 @@ def part1():
 
 
 def part2():
-    pass
+    graph = {}
+    state = {}
+    with open("input.txt", "r") as f:
+        lines = list(map(lambda s: s.strip(), f.readlines()))
+        for line in lines:
+            line = line.split(" -> ")
+            module = line[0].strip()
+            type = None
+            if module != "broadcaster":
+                type = module[0]
+                module = module[1:]
+            dests = line[1].split(", ")
+            graph[module] = dests
+            state[module] = {
+                "type": type,
+                "state": 0,
+                "input": {}
+            }
+
+    for item in graph.items():
+        for dest in item[1]:
+            if dest not in state:
+                state[dest] = {
+                    "type": type,
+                    "state": 0,
+                    "input": {}
+                }
+            if state[dest]["type"] == '&':
+                state[dest]["input"][item[0]] = 0
+    i = 0
+    res = {}
+    while True:
+        if len(res) == 4:
+            break
+        i += 1
+        q = collections.deque([("broadcaster", 0, "button")])
+        while q:
+            module, signal, prev_module = q.popleft()
+            if module == "broadcaster":
+                dests = graph[module]
+                for dest in dests:
+                    q.append((dest, signal, module))
+            elif state[module]["type"] == '%':
+                if signal == 0:
+                    if state[module]["state"] == 0:
+                        state[module]["state"] = 1
+                    elif state[module]["state"] == 1:
+                        state[module]["state"] = 0
+                    for dest in graph[module]:
+                        q.append((dest, state[module]["state"], module))
+            elif state[module]["type"] == '&':
+                input = state[module]["input"]
+                input[prev_module] = signal
+                all_high_signal = False
+                if module == "kh":
+                    if any(x == 1 for x in input.values()):
+                        for k, v in input.items():
+                            if v == 1:
+                                if k not in res:
+                                    res[k] = i
+                if all(x == 1 for x in input.values()):
+                    all_high_signal = True
+                if module in graph:
+                    for dest in graph[module]:
+                        if all_high_signal:
+                            q.append((dest, 0, module))
+                        else:
+                            q.append((dest, 1, module))
+            else:
+                raise Exception("Unknown type")
+
+    return math.lcm(*res.values())
 
 
 if __name__ == "__main__":
