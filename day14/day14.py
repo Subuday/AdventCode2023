750354cd43c612ac060fe52b86a53a7b9d29ab0e
Maksym Sutkovenko <msutkovenko@readdle.com>
2023-12-27 19:19:17 +0200

    Solve day14 part2

--- day14/day14.py
+++ day14/day14.py
@@ -23,8 +23,36 @@ def part1():
     return res
 
 
+def cycle(grid):
+    for _ in range(4):
+        grid = tuple(map("".join, zip(*grid)))
+        grid = tuple(
+            "#".join(["".join(sorted(tuple(group), reverse=True)) for group in row.split("#")]) for row in grid)
+        grid = tuple(row[::-1] for row in grid)
+    return grid
+
+
 def part2():
-    pass
+    grid = tuple(open("input.txt").read().splitlines())
+
+    seen = {grid}
+    array = [grid]
+
+    iter = 0
+
+    while True:
+        iter += 1
+        grid = cycle(grid)
+        if grid in seen:
+            break
+        seen.add(grid)
+        array.append(grid)
+
+    first = array.index(grid)
+
+    grid = array[(1000000000 - first) % (iter - first) + first]
+
+    print(sum(row.count("O") * (len(grid) - r) for r, row in enumerate(grid)))
 
 
 if __name__ == "__main__":
